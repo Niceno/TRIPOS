58362ead45270612c5821eaef16864afe502ca06
Niceno <bojan.niceno.scientist@gmail.com>
2019-05-09 22:59:51 +0200

    Added timing.

--- Sources/Mesh_Mod/Smooth.f90
+++ Sources/Mesh_Mod/Smooth.f90
@@ -6,6 +6,8 @@
   integer :: it, s, n, e
 !==============================================================================!
 
+  call Cpu_Timer_Mod_Start('Mesh_Mod_Smooth')
+
   do it = 0, 9
     do s = 0, n_side-1
       if(side(s) % mark .eq. 0) then
@@ -38,5 +40,7 @@
       call Mesh_Mod_Circles(e)
     end if
   end do
+
+  call Cpu_Timer_Mod_Stop('Mesh_Mod_Smooth')
 
   end subroutine
