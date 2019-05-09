06e24f898e2123090a28b01f9968e4f4d1a269f7
Niceno <bojan.niceno.scientist@gmail.com>
2019-05-09 22:54:39 +0200

    Fancier output.

--- Sources/Cpu_Timer_Mod/Statistics.f90
+++ Sources/Cpu_Timer_Mod/Statistics.f90
@@ -6,7 +6,7 @@
   integer           :: max_l, f
   character(len= 4) :: max_l_s
   real(RP)          :: total_time
-  character(len=CL) :: f_name
+  character(len=CL) :: line
 !==============================================================================!
 
   ! Find max function name length
@@ -15,21 +15,38 @@
     max_l = max(max_l, len_trim(funct_name(f)))
   end do
   write(max_l_s, "(i4.4)"), max_l+4
-
+  max_l = 40
 
   total_time = 0.0
   do f=1, n_funct
     total_time = total_time + funct_time(f)
   end do
 
+  print *, "#=======================================================#"
+  print *, "#                 CPU usage statistics                  #"
+  print *, "#-------------------------------------------------------#"
+  line( 1:CL) = " "
+  line( 1:29) = "#             Total CPU time: "
+  write(line(30:39), "(f9.3)") total_time
+  line(40:42) = "[s]"
+  line(57:57) = "#"
+  print *, trim(line)
+  print *, "#-------------------------------------------------------#"
+  print *, "# Function:                      Time:                  #"
+  print *, "#-------------------------------------------------------#"
+
   do f=1, n_funct
-    f_name(1:CL) = ""
-    f_name(2:2)  = "-"
-    f_name(4:len_trim(funct_name(f))+3) = funct_name(f)
-    write(*,"(a" // max_l_s //", a1, f8.3, a4, f8.1, a2)")  &
-             f_name(1:max_l+4),                  ":",       &
-             funct_time(f),                      " [s]",    &
-             funct_time(f) / total_time * 100.0, " %"
+    line( 1:CL) = " "
+    line( 1: 1) = "#"
+    line(57:57) = "#"
+    line( 3: 3+len_trim(funct_name(f))) = funct_name(f)
+    write(line(31:38), "(f8.3)") funct_time(f)
+    line(40:42) = "[s]"
+    write(line(47:50), "(f4.1)") funct_time(f) / total_time * 100.0
+    line(52:52) = "%"
+    print *, trim(line)
   end do
+
+  print *, "#=======================================================#"
 
   end subroutine
