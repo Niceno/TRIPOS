ef3e9175eb93cb6587c7ad9a76a8fefaea464bf7
Bojan Niceno <bojan@niceno.ch>
2019-05-28 09:55:20 +0200

    Updated compile.bat for Windows.

--- Sources/compile.bat
+++ Sources/compile.bat
@@ -1,5 +1,10 @@
 ifort -c Const_Mod.f90 
 ifort -c Cpu_Timer_Mod.f90 
+ifort -c Comm_Mod.f90 
 ifort -c Mesh_Mod.f90 
 ifort -c File_Mod.f90 
-ifort -o Easymesh_Fortran.exe *.obj Global\*.f90
+ifort -c Sort_Mod.f90 
+ifort -c Vector_Mod.f90
+ifort -c Matrix_Mod.f90
+ifort -c Solver_Mod.f90
+ifort -o Easymesh_Fortran.exe *.obj Global\*.f90 Main.f90
