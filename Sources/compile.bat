8ecbdce2c86dcb6b37d40f63286758338c26e608
Bojan Niceno <bojan@niceno.ch>
2019-06-04 12:02:01 +0200

    Corrected compile script!

--- Sources/compile.bat
+++ Sources/compile.bat
@@ -2,9 +2,9 @@ ifort -c Const_Mod.f90
 ifort -c Cpu_Timer_Mod.f90 
 ifort -c Comm_Mod.f90 
 ifort -c Mesh_Mod.f90 
-ifort -c File_Mod.f90 
 ifort -c Sort_Mod.f90 
 ifort -c Vector_Mod.f90
+ifort -c File_Mod.f90 
 ifort -c Matrix_Mod.f90
 ifort -c Solver_Mod.f90
 ifort -o Tripos.exe *.obj Global\*.f90 Main.f90
