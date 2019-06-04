3ee3334b2c463945722e247b4c378c349d79d388
Bojan Niceno <bojan@niceno.ch>
2019-06-04 11:34:12 +0200

    EasyMesh => Tripos!

--- Sources/compile.bat
+++ Sources/compile.bat
@@ -7,4 +7,4 @@ ifort -c Sort_Mod.f90
 ifort -c Vector_Mod.f90
 ifort -c Matrix_Mod.f90
 ifort -c Solver_Mod.f90
-ifort -o Easymesh_Fortran.exe *.obj Global\*.f90 Main.f90
+ifort -o Tripos.exe *.obj Global\*.f90 Main.f90
