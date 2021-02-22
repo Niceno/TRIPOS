1a087b45a2019a93805f8b896ed3fdddbb9ef08a
Niceno <bojan.niceno.scientist@gmail.com>
2021-02-22 17:22:39 +0100

    Nicer output when solving the problem.
    modified:   Main.f90

--- Sources/Main.f90
+++ Sources/Main.f90
@@ -14,14 +14,17 @@
 !------------------------------------------------------------------------------!
   implicit none
 !-----------------------------------[Locals]-----------------------------------!
-  type(Options_Type) :: opts
-  type(Mesh_Type)    :: mesh
-  integer            :: max_it, act_it, i
-  real(RP)           :: dt = 0.1, f_res, t_res = 1.0e-6
-  type(Matrix_Type)  :: a
-  type(Vector_Type)  :: phi, phi_o
-  type(Vector_Type)  :: b
-  type(Solver_Type)  :: krylov
+  type(Options_Type)      :: opts
+  type(Mesh_Type)         :: mesh
+  integer                 :: max_it, act_it, j
+  character(3), parameter :: I = '   '  ! indent for solver output
+  real(RP)                :: dt = 1.0   ! false time step
+  real(RP)                :: f_sol, t_sol = 1.0e-8
+  real(RP)                :: c_out, t_out = 1.0e-6
+  type(Matrix_Type)       :: a
+  type(Vector_Type)       :: phi_n, phi_o
+  type(Vector_Type)       :: b
+  type(Solver_Type)       :: krylov
 !==============================================================================!
 
   call Cpu_Timer_Mod_Start('Tripos_Main')
@@ -108,7 +111,7 @@
     !--------------------------!
     call Solver_Mod_Allocate(krylov, mesh)
     call Matrix_Mod_Allocate(a,   mesh)
-    call Vector_Mod_Allocate(phi,   mesh)
+    call Vector_Mod_Allocate(phi_n, mesh)
     call Vector_Mod_Allocate(phi_o, mesh)
     call Vector_Mod_Allocate(b,   mesh)
 
@@ -123,45 +126,74 @@
     !------------------------!
     max_it = mesh % n_node  ! set maximum number of iterations
 
-    do i = 1, 12000
+    ! Discretize linear system for the first time
+    call Discretize(a, phi_o, b, dt)
 
-      if(mod(i, 20) .eq. 0 .and. opts % messages .eq. ON) then
-        print "(a,i5,es12.4)", " Current time step: ", i, dt
+    print "(a)", ""
+    print "(a,a)", I, "#=================================" // &
+                      "==================================#"
+    print "(a,a,es10.3,a)", I, "# Starting iterative procedure " // &
+                               "with target residual: ",  t_out, "     #"
+    print "(a,a)", I, "#---------------------------------" // &
+                      "----------------------------------#"
+    do j = 1, 100000
+
+      if(mod(j, 20) .eq. 0 .and. opts % messages .eq. ON) then
+        print "(a,a,i5,a,2es10.3,a)", I, "# Time step:", j,  &
+              "; solver and outer residuals: ", f_sol, c_out, " #"
       end if
 
       ! Actual value becomes old
-      phi_o % val(:) = phi % val(:)
+      phi_o % val(:) = phi_n % val(:)
 
       ! Discretize linear system and update r.h.s.
       call Discretize(a, phi_o, b, dt)
 
       ! Cal linear solver
-      ! call Solver_Mod_Bicg(krylov, a, phi, b, max_it, act_it, t_res, f_res)
-      call Solver_Mod_Cg(krylov, a, phi, b, max_it, act_it, t_res, f_res)
+      ! call Solver_Mod_Bicg(krylov, a, phi_n, b, max_it, act_it, t_sol, f_sol)
+      call Solver_Mod_Cg(krylov, a, phi_n, b, max_it, act_it, t_sol, f_sol)
+
+      ! Find current residual for outer iterations
+      c_out = norm2((phi_n % val - phi_o % val))
+
+      if(act_it .eq. 0) then
+        print "(a,a)", I, "#---------------------------------" // &
+                          "----------------------------------#"
+        print "(a,a,i5,a)", I, "# Solver can't converge " // &
+                               "any further at time step: ",  &
+                               j, "             #"
+        goto 1
+      end if
+
+      if(c_out < t_out) then
+        print "(a,a)", I, "#---------------------------------" // &
+                          "----------------------------------#"
+        print "(a, a,i5,a,es10.3,a)", I, "# Convergence reached at time step ",&
+                                      j, " with residual   ", c_out, " #"
+        goto 1
+      end if
+
+      if(mod(j, 20) .eq. 0 .and. opts % messages .eq. ON) then
+      end if
 
       ! Gradually increase time step, you want steady solution anyway
       dt = dt * 1.2
 
-      if(act_it .eq. 0) then
-        goto 1
-      end if
-
-      if(mod(i, 20) .eq. 0 .and. opts % messages .eq. ON) then
-        print "(a,i5,es12.4)", " Solver iterations: ", act_it, f_res
-      end if
     end do
 1   continue
+    print "(a,a)", I, "#=================================" // &
+                      "==================================#"
 
     !-------------------------------------!
     !   Plot results in DXF file format   !
     !-------------------------------------!
     if(opts % dxf .eq. ON) then
-      call File_Mod_Dxf_Start_Results(phi, opts)
-      call File_Mod_Dxf_Draw_Results(phi)
+      call File_Mod_Dxf_Start_Results(phi_n, opts)
+      call File_Mod_Dxf_Draw_Results(phi_n)
       call File_Mod_Dxf_End
 
-      call File_Mod_Dxf_Start_Gradients(phi, opts)
-      call File_Mod_Dxf_Draw_Gradients(phi, opts)
+      call File_Mod_Dxf_Start_Gradients(phi_n, opts)
+      call File_Mod_Dxf_Draw_Gradients(phi_n, opts)
       call File_Mod_Dxf_End
     end if
 
@@ -170,11 +202,11 @@
     !-------------------------------------!
     if(opts % eps .eq. ON) then
       call File_Mod_Eps_Start_Results(opts)
-      call File_Mod_Eps_Draw_Results(phi)
+      call File_Mod_Eps_Draw_Results(phi_n)
       call File_Mod_Eps_End
 
       call File_Mod_Eps_Start_Gradients(opts)
-      call File_Mod_Eps_Draw_Gradients(phi, opts)
+      call File_Mod_Eps_Draw_Gradients(phi_n, opts)
       call File_Mod_Eps_End
     end if
   end if
